d5df8e5a41acece54e25cada2bd4db79175882a4
유현 <cadry238@gmail.com>
2026-04-28 22:46:17 +0900

    기초 작업 세팅 완료

---
+++ src/main/java/com/dondoc/DondocBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dondoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DondocBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DondocBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dondoc/DondocBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dondoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DondocBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
